c4857e5d739a51e8d9787c53f7acb94adfd93d35
jcradit <jasoncradit@gmail.com>
2016-03-04 10:55:22 -0700

    now apache

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,9 @@
-name             'tomcat7'
-maintainer       'Stephen Carman'
-maintainer_email 'scarman@coldlight.com'
+name             'apache'
+maintainer       'Jason Cradit'
+maintainer_email 'mine@mine.com'
 license          'Apache 2.0'
-description      'Installs/Configures Tomcat'
+description      'Installs/starts Apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
 supports "redhat"
-supports "centos"
-supports "amazon"
-supports "fedora"
-
-#depends "java"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,5 @@
+package 'httpd'
 
-yum_package node['tomcat']['package'] do
-	action :install
-end
-
-service node['tomcat']['service'] do
-	action 	[ :enable, :start ]
+service 'httpd' do
+  action [:enable, :start]
 end
