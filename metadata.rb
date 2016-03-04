cd5b0c52ad91e12436898aa37dfceea58d375a38
jcradit <jasoncradit@gmail.com>
2016-03-04 12:26:05 -0700

    cleaned up

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Jason Cradit'
 maintainer_email 'mine@mine.com'
 license          'Apache 2.0'
 description      'Installs/starts Apache'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description 'installs apache'
 version          '0.1.2'
 
 supports "redhat"
